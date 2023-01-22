389c683bd24e6061e4acecf0d044f4604b4fede1
Yu Cheng Li <97200083+ycheng11065@users.noreply.github.com>
2023-01-22 19:49:48 +0000

    Almost final

--- main.py
+++ main.py
@@ -31,39 +31,28 @@ ALL_BUTTONS = [ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN]
 
 my_secret = os.environ['key']  # Our token for discord bot to run
 
-class NaviBot(commands.Bot):
+def create_embed(curr_menu, author):
+  embed_to_return = discord.Embed(title=curr_menu.title)
+  embed_to_return.set_footer(text=f'Hi {author}')
+  for field in curr_menu.list_selection:
+    embed_to_return.add_field(name=field["field_name"],
+                              value=field["field_value"],
+                              inline=field["field_inline"])
+  return embed_to_return
 
-  def __init__(self, ):
-  additional_buttons = {}
-  add_button(additional_buttons, MENU_BUTTON_NAME, MENU_ICON)
-  add_button(additional_buttons, BACK_BUTTON_NAME, BACK_ICON)
+def add_button(dict_buttons, button_name, button_emoji):
+  dict_buttons[button_name] = button_emoji
 
-  #helper functions
-  def create_embed(node, author):
-    embed_to_return = discord.Embed(title=node.title)
-    for field in node.list_selection:
-      embed_to_return.add_field(name=field["field_name"],
-                                value=field["field_value"],
-                                inline=field["field_inline"])
-    embed_to_return.set_footer(text=f'Hi {author}')
-    
-    return embed_to_return
-
-  def add_button(dict_buttons, button_name, button_emoji):
-    dict_buttons[button_name] = button_emoji
-
-
+additional_buttons = {}
+add_button(additional_buttons, MENU_BUTTON_NAME, MENU_ICON)
+add_button(additional_buttons, BACK_BUTTON_NAME, BACK_ICON)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-activity = discord.Activity(type=discord.ActivityType.watching, name="for someone to type !menu")
-
 bot = commands.Bot(command_prefix='!',
-                   activity=activity,
                    intents=intents,
-                   help_command=EmbedHelpCommand(), 
-                   status=discord.Status.idle)
+                   help_command=EmbedHelpCommand())
 
 
 @bot.command()
@@ -78,6 +67,76 @@ async def menu(ctx):
   number_buttons = ALL_BUTTONS[0:curr_num_buttons]
 
   curr_msg = await ctx.send(embed=curr_embed)
+  
+
+  # loop while execution (might wanna set an ending condition / command)
+  while True:
+
+    # 1) display the embed and add buttons
+    curr_msg = await curr_msg.edit(embed=curr_embed)
+
+    for button in additional_buttons.values():
+        await curr_msg.add_reaction(button)
+      
+    for button in number_buttons:
+        await curr_msg.add_reaction(button)
+
+    # 2) get the user's reaction and fetch the according next node
+    try:
+        reaction, user = await bot.wait_for(
+            "reaction_add",
+            check=lambda reaction, user: (user == ctx.author) and ((reaction.emoji in number_buttons) or (reaction.emoji in additional_buttons.values())),
+            timeout=60.0)
+
+    except asyncio.TimeoutError:
+        embed = create_embed(curr_node, ctx.author)
+        embed.set_footer(text="Timed Out.")
+        await curr_msg.clear_reactions()
+    
+    else:
+
+        user_reaction_num = None
+
+        # process the additional buttons FIRST
+        if (reaction.emoji == additional_buttons[BACK_BUTTON_NAME]):
+          if len(stack) != 0:
+            next_node = stack.pop()
+          else:
+            next_node = curr_node
+        
+        elif (reaction.emoji == additional_buttons[MENU_BUTTON_NAME]):
+          next_node = files_manager.return_root_of_tree()
+          print(next_node.title)
+          stack.append(curr_node)
+
+        else: 
+          for i in range(len(number_buttons)):
+              if reaction.emoji == number_buttons[i]:
+                user_reaction_num = i
+                break 
+          next_node = curr_node.list_children[user_reaction_num]
+          stack.append(curr_node)
+      
+    # 3) update the current node to the next node, update embed, and set the number of buttons (= the number of its children)
+        curr_node = next_node
+        curr_embed = create_embed(curr_node, ctx.author)
+        curr_num_buttons = len(curr_node.list_children)
+        new_number_buttons = ALL_BUTTONS[0:curr_num_buttons]
+
+      
+    # 4) remove all buttons that are not needed
+        for button in (list(additional_buttons.values()) + number_buttons):
+            await curr_msg.remove_reaction(button, ctx.author)
+        for button in number_buttons:
+          if button not in new_number_buttons:
+            await curr_msg.remove_reaction(button, bot.user)
+
+          # if (len(number_buttons) != len(curr_node.list_children)):
+
+        number_buttons = new_number_buttons
+            
+
+        
 
 
 @bot.event
@@ -99,79 +158,33 @@ async def on_message(message):
   await bot.process_commands(message)
 
 
-@bot.event
-async def on_reaction_add(reaction, user):
-  # 0) if reaction by the bot itself, ignore
-  if user.author == bot.author:
-    return
-  
-  # 1) display the embed and add buttons
-  curr_msg = await curr_msg.edit(embed=curr_embed)
+# @bot.event
+# async def on_reaction_add(reaction, user):
+#   # if user.author == bot.author:
+#   #   return
+#   # print('Curr page?: ', reaction.message == curr_page)
+#   # print('Bot?: ', reaction.author == bot.user)
+#   if reaction.emoji == ONE and reaction.message == curr_page:
+#     faculty = files_manager.return_root_of_tree()
+#     faculty_embed = create_embed(faculty)
+#     # await curr_page.remove_reaction(ONE, curr_page.author)
+#     # await curr_page.remove_reaction(TWO, curr_page.author)
+#     # await curr_page.remove_reaction(THREE, curr_page.author)
+#     # await curr_page.edit(embed=faculty_embed)
 
-  for button in additional_buttons.values():
-      await curr_msg.add_reaction(button)
-    
-  for button in number_buttons:
-      await curr_msg.add_reaction(button)
+#     await reaction.message.remove_reaction(ONE, curr_page.author)
+#     await reaction.message.remove_reaction(TWO, curr_page.author)
+#     await reaction.message.remove_reaction(THREE, curr_page.author)
+#     await reaction.message.edit(embed=faculty_embed)
 
-  # 2) get the user's reaction and fetch the according next node
-  try:
-      reaction, user = await bot.wait_for(
-          "reaction_add",
-          check=lambda reaction, user: (user == ctx.author) and ((reaction.emoji in number_buttons) or (reaction.emoji in additional_buttons.values())),
-          timeout=60.0)
+#   elif reaction.emoji == TWO:
+#     return
 
-  except asyncio.TimeoutError:
-      embed = create_embed(curr_node, ctx.author)
-      embed.set_footer(text="Timed Out.")
-      await curr_msg.clear_reactions()
-  
-  else:
-
-      user_reaction_num = None
-
-      # process the additional buttons FIRST
-      if (reaction.emoji == additional_buttons[BACK_BUTTON_NAME]):
-        if len(stack) != 0:
-          next_node = stack.pop()
-        else:
-          next_node = curr_node
-      
-      elif (reaction.emoji == additional_buttons[MENU_BUTTON_NAME]):
-        next_node = files_manager.return_root_of_tree()
-        print(next_node.title)
-        stack.append(curr_node)
-
-      else: 
-        for i in range(len(number_buttons)):
-            if reaction.emoji == number_buttons[i]:
-              user_reaction_num = i
-              break 
-        next_node = curr_node.list_children[user_reaction_num]
-        stack.append(curr_node)
-    
-  # 3) update the current node to the next node, update embed, and set the number of buttons (= the number of its children)
-      curr_node = next_node
-      curr_embed = create_embed(curr_node, ctx.author)
-      curr_num_buttons = len(curr_node.list_children)
-      new_number_buttons = ALL_BUTTONS[0:curr_num_buttons]
-
-    
-  # 4) remove all buttons that are not needed
-      for button in (list(additional_buttons.values()) + number_buttons):
-          await curr_msg.remove_reaction(button, ctx.author)
-      for button in number_buttons:
-        if button not in new_number_buttons:
-          await curr_msg.remove_reaction(button, bot.user)
-
-        # if (len(number_buttons) != len(curr_node.list_children)):
-
-      number_buttons = new_number_buttons
-      
+#   elif reaction.emoji == THREE:
+#     return
 
 
-if __name__ == "__main__":
-  try:
-    bot.run(my_secret)
-  except:
-    os.system("kill 1")
+try:
+  bot.run(my_secret)
+except:
+  os.system("kill 1")
--- main_changes_made.py
+++ main_changes_made.py
@@ -32,15 +32,14 @@ ALL_BUTTONS = [ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN]
 my_secret = os.environ['key']  # Our token for discord bot to run
 
 class NaviBot(commands.Bot):
-
-  def __init__(self, *args, **kwargs):
-    super(NaviBot, self).__init__(*args)
+  additional_buttons = None
+  curr_node = None
+  numer_buttons = []
+  stack = []
+  curr_msg = None
     
-  additional_buttons = {}
-  add_button(additional_buttons, MENU_BUTTON_NAME, MENU_ICON)
-  add_button(additional_buttons, BACK_BUTTON_NAME, BACK_ICON)
-
   #helper functions
+  @staticmethod
   def create_embed(node, author):
     embed_to_return = discord.Embed(title=node.title)
     for field in node.list_selection:
@@ -51,128 +50,150 @@ class NaviBot(commands.Bot):
     
     return embed_to_return
 
+  #helper functions 2
+  @staticmethod
   def add_button(dict_buttons, button_name, button_emoji):
     dict_buttons[button_name] = button_emoji
 
+  # Returns very first page
+  @staticmethod
+  def getFirstPage():
+    files_manager.return_root_of_tree()
 
+  # Sets up the bot such that it has additional buttons, current node and 
+  # other attributes initialized; should run each time we call menu
+  def setUp(self):
+    #Add additional buttons
+    self.additional_buttons = {}
+    NaviBot.add_button(self.additional_buttons, MENU_BUTTON_NAME, MENU_ICON)
+    NaviBot.add_button(self.additional_buttons, BACK_BUTTON_NAME, BACK_ICON)
 
-intents = discord.Intents.default()
-intents.message_content = True
+    #Setup the current node
+    self.curr_node = NaviBot.getFirstPage()
 
-activity = discord.Activity(type=discord.ActivityType.watching, name="for someone to type !menu")
+    #Setup the stack
+    self.stack = []
 
-bot = commands.Bot(command_prefix='!',
-                   activity=activity,
-                   intents=intents,
-                   help_command=EmbedHelpCommand(), 
-                   status=discord.Status.idle)
-
-
-@bot.command()
-async def menu(ctx):
-  # 0) setting up: starting note is set to the root of tree
-  #  & update the current node's embed, and set the set of buttons (= the number of its children)
-  stack = []
-  curr_node = files_manager.return_root_of_tree()
-  curr_embed = create_embed(curr_node, ctx.author)
-  curr_num_buttons = len(curr_node.list_children)
-  
-  number_buttons = ALL_BUTTONS[0:curr_num_buttons]
-
-  curr_msg = await ctx.send(embed=curr_embed)
-
-
-@bot.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(bot))
-  # First menu here
-
-
-@bot.event
-async def on_message(message):
-  if message.author == bot.user:
-    return
-
-  print(f'Message from {message.author}: {message.content}')
-
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
-
-  await bot.process_commands(message)
-
-
-@bot.event
-async def on_reaction_add(reaction, user):
-  # 0) if reaction by the bot itself, ignore
-  if user.author == bot.author:
-    return
-  
-  # 1) display the embed and add buttons
-  curr_msg = await curr_msg.edit(embed=curr_embed)
-
-  for button in additional_buttons.values():
-      await curr_msg.add_reaction(button)
+  @bot.command()
+  async def menu(self, ctx):
+    # 0) setting up: starting note is set to the root of tree
+    #  & update the current node's embed, and set the set of buttons (= the number of its children)
+    self.setUp()
+    curr_embed = NaviBot.create_embed(self.curr_node, ctx.author)
+    curr_num_buttons = len(self.curr_node.list_children)
     
-  for button in number_buttons:
-      await curr_msg.add_reaction(button)
-
-  # 2) get the user's reaction and fetch the according next node
-  try:
-      reaction, user = await bot.wait_for(
-          "reaction_add",
-          check=lambda reaction, user: (user == ctx.author) and ((reaction.emoji in number_buttons) or (reaction.emoji in additional_buttons.values())),
-          timeout=60.0)
-
-  except asyncio.TimeoutError:
-      embed = create_embed(curr_node, ctx.author)
-      embed.set_footer(text="Timed Out.")
-      await curr_msg.clear_reactions()
+    self.number_buttons = ALL_BUTTONS[0:curr_num_buttons]
   
-  else:
+    self.curr_msg = await ctx.send(embed=curr_embed)
 
-      user_reaction_num = None
 
-      # process the additional buttons FIRST
-      if (reaction.emoji == additional_buttons[BACK_BUTTON_NAME]):
-        if len(stack) != 0:
-          next_node = stack.pop()
-        else:
-          next_node = curr_node
+  @bot.event
+  async def on_ready(self):
+    print('We have logged in as {0.user}'.format(bot))
+    
+    # First menu here?
+
+
+  @bot.event
+  async def on_message(self, message):
+    if message.author == bot.user:
+      return
+  
+    print(f'Message from {message.author}: {message.content}')
+  
+    if message.content.startswith('$hello'):
+      await message.channel.send('Hello!')
+  
+    await bot.process_commands(message)
+
+
+  @bot.event
+  async def on_reaction_add(self, reaction, user):
+    # 0) if reaction by the bot itself, ignore
+    if user.author == bot.author:
+      return
+    # 0.2) if self.curr_msg is None, ignore
+    if self.curr_msg == None:
+      return
       
-      elif (reaction.emoji == additional_buttons[MENU_BUTTON_NAME]):
-        next_node = files_manager.return_root_of_tree()
-        print(next_node.title)
-        stack.append(curr_node)
-
-      else: 
-        for i in range(len(number_buttons)):
-            if reaction.emoji == number_buttons[i]:
-              user_reaction_num = i
-              break 
-        next_node = curr_node.list_children[user_reaction_num]
-        stack.append(curr_node)
+    # 1) display the embed and add buttons
+    curr_embed = self.curr_node
+    self.curr_msg = await self.curr_msg.edit(embed=curr_embed)
+  
+    for button in self.additional_buttons.values():
+        await curr_msg.add_reaction(button)
+      
+    for button in self.number_buttons:
+        await curr_msg.add_reaction(button)
+  
+    # 2) get the user's reaction and fetch the according next node
+    try:
+        reaction, user = await bot.wait_for(
+            "reaction_add",
+            check=lambda reaction, user: (user == ctx.author) and ((reaction.emoji in self.number_buttons) or (reaction.emoji in self.additional_buttons.values())),
+            timeout=60.0)
+  
+    except asyncio.TimeoutError:
+        embed = NaviBot.create_embed(self.curr_node, ctx.author)
+        embed.set_footer(text="Timed Out.")
+        await curr_msg.clear_reactions()
     
-  # 3) update the current node to the next node, update embed, and set the number of buttons (= the number of its children)
-      curr_node = next_node
-      curr_embed = create_embed(curr_node, ctx.author)
-      curr_num_buttons = len(curr_node.list_children)
-      new_number_buttons = ALL_BUTTONS[0:curr_num_buttons]
-
-    
-  # 4) remove all buttons that are not needed
-      for button in (list(additional_buttons.values()) + number_buttons):
-          await curr_msg.remove_reaction(button, ctx.author)
-      for button in number_buttons:
-        if button not in new_number_buttons:
-          await curr_msg.remove_reaction(button, bot.user)
-
-        # if (len(number_buttons) != len(curr_node.list_children)):
-
-      number_buttons = new_number_buttons
+    else:
+  
+        user_reaction_num = None
+  
+        # process the additional buttons FIRST
+        if (reaction.emoji == self.additional_buttons[BACK_BUTTON_NAME]):
+          if len(self.stack) != 0:
+            next_node = self.stack.pop()
+          else:
+            next_node = self.curr_node
+        
+        elif (reaction.emoji == self.additional_buttons[MENU_BUTTON_NAME]):
+          next_node = getFirstPage()
+          self.stack.append(self.curr_node)
+  
+        else: 
+          for i in range(len(self.number_buttons)):
+              if reaction.emoji == self.number_buttons[i]:
+                user_reaction_num = i
+                break 
+          next_node = self.curr_node.list_children[user_reaction_num]
+          self.stack.append(self.curr_node)
+      
+    # 3) update the current node to the next node, update embed, and set the number of buttons (= the number of its children)
+        self.curr_node = next_node
+        curr_embed = NaviBot.create_embed(self.curr_node, ctx.author)
+        curr_num_buttons = len(self.curr_node.list_children)
+        new_number_buttons = ALL_BUTTONS[0:curr_num_buttons]
+  
+      
+    # 4) remove all buttons that are not needed
+        for button in (list(self.additional_buttons.values()) + self.number_buttons):
+            await self.curr_msg.remove_reaction(button, ctx.author)
+        for button in self.number_buttons:
+          if button not in new_number_buttons:
+            await self.curr_msg.remove_reaction(button, bot.user)
+  
+          # if (len(number_buttons) != len(curr_node.list_children)):
+  
+        self.number_buttons = new_number_buttons
       
 
 
 if __name__ == "__main__":
+  
+  intents = discord.Intents.default()
+  intents.message_content = True
+
+  activity = discord.Activity(type=discord.ActivityType.watching, name="for someone to type !menu")
+
+  bot = commands.Bot(command_prefix='!',
+                     activity=activity,
+                     intents=intents,
+                     help_command=EmbedHelpCommand(), 
+                     status=discord.Status.idle)
+  
   try:
     bot.run(my_secret)
   except:
