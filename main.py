47ee6694a4215999c38827c2b776c6aa60e56e5a
Yu Cheng Li <97200083+ycheng11065@users.noreply.github.com>
2023-01-22 17:58:20 +0000

    Base idea completed

--- main.py
+++ main.py
@@ -20,8 +20,14 @@ EIGHT = "8️⃣"
 NINE = "9️⃣"
 TEN = "🔟"
 
+BACK_ICON = ARROW_LEFT
+MENU_ICON = "⏪"
+
+BACK_BUTTON_NAME = "back_button"
+MENU_BUTTON_NAME = "menu_button"
+NUMBER_BUTTONS_NAME = "number_buttons"
+
 ALL_BUTTONS = [ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN]
-BUTTONS = [ONE, TWO, THREE]
 
 my_secret = os.environ['key']  # Our token for discord bot to run
 
@@ -33,6 +39,9 @@ def create_embed(curr_menu):
                               inline=field["field_inline"])
   return embed_to_return
 
+def add_button(dict_buttons, button_name, button_emoji):
+  dict_buttons[button_name] = button_emoji
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -40,90 +49,88 @@ bot = commands.Bot(command_prefix='!',
                    intents=intents,
                    help_command=EmbedHelpCommand())
 
+
 @bot.command()
 async def menu(ctx):
-  node_currently_in = files_manager.return_root_of_tree()
-  num_buttons = 1
-  msg = await ctx.send(embed=create_embed(node_currently_in))
+  # 0) setting up: starting note is set to the root of tree
+  #  & update the current node's embed, and set the set of buttons (= the number of its children)
+  stack = []
+  curr_node = files_manager.return_root_of_tree()
+  curr_embed = create_embed(curr_node)
+  curr_num_buttons = len(curr_node.list_children)
   
+  number_buttons = ALL_BUTTONS[0:curr_num_buttons]
+  additional_buttons = {}
+  add_button(additional_buttons, MENU_BUTTON_NAME, MENU_ICON)
+  add_button(additional_buttons, BACK_BUTTON_NAME, BACK_ICON)
+
+  curr_msg = await ctx.send(embed=curr_embed)
+
+  # loop while execution (might wanna set an ending condition / command)
   while True:
-    
-    emoji_button = ALL_BUTTONS[0:num_buttons]
-    
-    bot.help_pages = []
-    for child in node_currently_in.list_children:
-      bot.help_pages.append(create_embed(child)) 
-  
-    for button in emoji_button:
-      await msg.add_reaction(button)
-  
+
+    # 1) display the embed and add buttons
+    curr_msg = await curr_msg.edit(embed=curr_embed)
+
+    for button in additional_buttons.values():
+        await curr_msg.add_reaction(button)
+      
+    for button in number_buttons:
+        await curr_msg.add_reaction(button)
+
+    # 2) get the user's reaction and fetch the according next node
     try:
-      reaction, user = await bot.wait_for(
-        "reaction_add",
-        check=lambda reaction, user: user == ctx.author and reaction.emoji in
-        emoji_button,
-        timeout=60.0)
+        reaction, user = await bot.wait_for(
+            "reaction_add",
+            check=lambda reaction, user: (user == ctx.author) and ((reaction.emoji in number_buttons) or (reaction.emoji in additional_buttons.values())),
+            timeout=60.0)
 
     except asyncio.TimeoutError:
-      embed = create_embed(child)
-      embed.set_footer(text="Timed Out.")
-      await msg.clear_reactions()
-
+        embed = create_embed(curr_node)
+        embed.set_footer(text="Timed Out.")
+        await curr_msg.clear_reactions()
+    
     else:
-      next_page_index = 0
-      
-      for i in range(len(emoji_button)):
-        if reaction.emoji == emoji_button[i]:
-          next_page_index = i
-          break
 
-      for button in emoji_button:
-        await msg.remove_reaction(button, ctx.author)
+        user_reaction_num = None
 
-      node_currently_in = node_currently_in.list_children[next_page_index]
-      num_buttons = len(node_currently_in.list_children)
-      await msg.edit(embed=bot.help_pages[next_page_index])
+        # process the additional buttons FIRST
+        if (reaction.emoji == additional_buttons[BACK_BUTTON_NAME]):
+          next_node = stack.pop()
         
+        elif (reaction.emoji == additional_buttons[MENU_BUTTON_NAME]):
+          next_node = files_manager.return_root_of_tree()
+          print(next_node.title)
+          stack.append(curr_node)
 
-
-# async def menu(ctx):
-#   emoji_button = ALL_BUTTONS[0:3]
-#   current = 0
-#   msg = await ctx.send(embed=bot.help_pages[current])
-
-#   for button in emoji_button:
-#     await msg.add_reaction(button)
-
-#   while True:
-#     try:
-#       reaction, user = await bot.wait_for(
-#         "reaction_add",
-#         check=lambda reaction, user: user == ctx.author and reaction.emoji in
-#         emoji_button,
-#         timeout=60.0)
-
-#     except asyncio.TimeoutError:
-#       embed = bot.help_pages[current]
-#       embed.set_footer(text="Timed Out.")
-#       await msg.clear_reactions()
-
-#     else:
-#       previous_page = current
+        else: 
+          for i in range(len(number_buttons)):
+              if reaction.emoji == number_buttons[i]:
+                user_reaction_num = i
+                break 
+          next_node = curr_node.list_children[user_reaction_num]
+          stack.append(curr_node)
       
-#       if reaction.emoji == ONE:
-#         current = 0
+    # 3) update the current node to the next node, update embed, and set the number of buttons (= the number of its children)
+        curr_node = next_node
+        curr_embed = create_embed(curr_node)
+        curr_num_buttons = len(curr_node.list_children)
+        new_number_buttons = ALL_BUTTONS[0:curr_num_buttons]
 
-#       elif reaction.emoji == TWO:
-#         current = 1
+      
+    # 4) remove all buttons that are not needed
+        for button in (list(additional_buttons.values()) + number_buttons):
+            await curr_msg.remove_reaction(button, ctx.author)
+        for button in number_buttons:
+          if button not in new_number_buttons:
+            await curr_msg.remove_reaction(button, bot.user)
 
-#       elif reaction.emoji == THREE:
-#         current = 2
+          # if (len(number_buttons) != len(curr_node.list_children)):
 
-#       for button in emoji_button:
-#         await msg.remove_reaction(button, ctx.author)
+        number_buttons = new_number_buttons
+            
 
-#       if current != previous_page:
-#         await msg.edit(embed=bot.help_pages[current])
+        
 
 
 @bot.event
@@ -169,88 +176,6 @@ async def on_message(message):
 
 #   elif reaction.emoji == THREE:
 #     return
-
-
-numbers = [1, 2, 3]
-
-
-# @bot.command()
-# async def menu(ctx):
-#   global curr_page
-#   #initial
-#   custom = discord.Embed(
-#     title='Menu',
-#     description="This country is not supported, you can ask me to add it" +
-#     "[here](https://degree-navigator.as.it.ubc.ca/dn4v/Login/dagdisclaimer.asp).",
-#     color=discord.Colour.blue(),
-#     url='https://brand.ubc.ca/files/2018/09/Logos_1_2CrestDownload_768px.jpg')
-
-#   custom.add_field(name='Course Navigation',
-#                    value='type 1 to start navigating your degree',
-#                    inline=False)
-
-#   custom.add_field(name='!greetings', value='type 2', inline=False)
-
-#   custom.add_field(name='!farewell', value='type 3', inline=False)
-#   msg = await ctx.channel.send(embed=custom)
-#   curr_page = msg
-
-#   for react in BUTTONS:
-#     await msg.add_reaction(react)
-
-  # while True:
-  #   try:
-  #     reaction, user = await bot.wait_for(
-  #       "reaction_add",
-  #       check=lambda reaction, user: user == ctx.author and reaction.emoji in
-  #       BUTTONS,
-  #       timeout=60.0)
-
-  #   except asyncio.TimeoutError:
-  #     embed = custom
-  #     embed.set_footer(text="Timed Out.")
-  #     await msg.clear_reactions()
-
-
-
-
-#Current commands
-# @bot.command()
-# async def faculty(ctx):
-#   await windows.faculty(ctx)
-
-# @bot.command()
-# async def types_of_specialization(ctx):
-#   await windows.types_of_specialization(ctx)
-
-# @bot.command()
-# async def major_select_one(ctx):
-#   await windows.major_select_one(ctx)
-
-# @bot.command()
-# async def major_summary(ctx):
-#   await windows.major_summary(ctx)
-
-# @bot.command()
-# async def major_general_regs(ctx):
-#   await windows.major_general_regs(ctx)
-
-@bot.command()
-async def communication_req(ctx):
-  await windows.communication_req(ctx)
-
-# @bot.command()
-# async def science_and_art(ctx):
-#   await windows.science_and_art(ctx)
-
-# @bot.command()
-# async def science_breadth_major(ctx):
-#   await windows.science_breadth_major(ctx)
-
-# @bot.command()
-# async def promotional_req(ctx):
-#   await windows.promotional_req(ctx)
-
 
 
 try:
